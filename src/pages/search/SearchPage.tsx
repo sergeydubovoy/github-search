72ca4b7738b6615c85c5854594d4186c638c071b
Sergey Dubovoy <sergeydubovoi@gmail.com>
2024-02-22 16:31:41 +0300

    Minor fix

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -74,7 +74,6 @@ export const SearchPage: React.FC = observer(() => {
       <SelectorPerPage onChange={handlePerPage} />
       <RepositoriesListWrapper>
         <ListWrapper>
-          <Loader />
           <ListTitle>{LIST_TITLES.result}</ListTitle>
           {MainStore.isLoading ? <Loader /> : <RepositoriesList />}
           {MainStore.repositories.length === 0 &&
